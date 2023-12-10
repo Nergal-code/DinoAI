01da380d54b97dc617bd25aad7a26253bc96400c
Caio Lima <53834100+Nergal-code@users.noreply.github.com>
2023-12-10 15:37:46 -0300

    Delete DinoAI-main directory
    Wrong Commit

--- DinoAI-main/controls.js
+++
@@ -1,7 +0,0 @@
-export default {
-  jump: new KeyboardEvent('keydown', { key: 'Space', keyCode: 32 }),
-  crounch: new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40 }),
-  dispatch(event) {
-    document.dispatchEvent(this[event]);
-  }
-}
